3769cfcea5e4f54d39844333bdf0d1805d8663e4
Chiranjeevi GK <chiranjeevibmse@gmail.com>
2018-10-31 18:26:40 +0530

    config server project

---
+++ ems-config-server/src/main/java/com/saartha/springclould/ems/config/EmployeeManagementConfigApplication.java
@@ -0,0 +1,12 @@
+package com.saartha.springclould.ems.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementConfigApplication.class, args);
+	}
+}
