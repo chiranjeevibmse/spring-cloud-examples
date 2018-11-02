a1f8e2d899c79e987cb6dec49bdc4043d9f0a49b
Chiranjeevi GK <chiranjeevibmse@gmail.com>
2018-11-02 14:32:40 +0530

    Added config server

---
+++ ems-config-server/src/main/java/com/saartha/springclould/ems/config/EmsConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.saartha.springclould.ems.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsConfigServerApplication.class, args);
+	}
+}
